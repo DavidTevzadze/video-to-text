0cc2270b441ee4bf380173b869de3d4817d63e0b
Evan Bulatoff <ngoldo@gmail.com>
2024-08-02 00:13:56 +0300

    feat: Add OpenAIHandler class for working with OpenAI API

--- app/openai_handler.py
+++ app/openai_handler.py
@@ -1,13 +1,29 @@
 """Модуль работы с OpenAI API"""
 from openai import OpenAI
 
+class OpenAIHandler:
+    """Класс для работы с OpenAI API"""
+    def __init__(self):
+        self.client = OpenAI()
 
-client = OpenAI()
+    def get_transcription(self, file_path: str) -> str:
+        """Получаем транскрипцию аудиофайла"""
+        audio_file = open(file_path, "rb")
+        transcription = self.client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file,
+            response_format="text"
+        )
+        return transcription.text
 
-audio_file = open("/path/to/file/speech.mp3", "rb")
-transcription = client.audio.transcriptions.create(
-    model="whisper-1",
-    file=audio_file,
-    response_format="text"
-)
-print(transcription.text)
+    def get_completion(self, request: str) -> str:
+        """Получаем ответ на сообщение пользователя"""
+        completion = self.client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "user", "content": request}
+            ]
+        )
+        if completion.choices[0].message.content is None:
+            return ("No completion")
+        return (completion.choices[0].message.content)
